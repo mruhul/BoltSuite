cad9bc25dedeadcdd97bc0c15ec41d7f80ee3b10
RuhulAmin <mruhul@gmail.com>
2020-05-28 10:03:47 +1000

    Refactor test

--- Bolt.RequestBus.Tests/RequestBusPublishTests.cs
+++ Bolt.RequestBus.Tests/RequestBusPublishTests.cs
@@ -8,54 +8,57 @@ namespace Bolt.RequestBus.Tests
 {
     public class RequestBusPublishTests
     {
-        [Fact]
-        public void Publish_Should_Execute_All_Applicable_EventHandlers()
+        public class Publish
         {
-            var sut = IocHelper.GetRequestBus(sc =>
+            [Fact]
+            public void Should_Execute_All_Applicable_EventHandlers()
             {
-                sc.AddTransient<IEventHandler<OrderPlaced>, OrderPlacedTest1EventHandler>();
-                sc.AddTransient<IEventHandler<OrderPlaced>, OrderPlacedTest2EventHandler>();
-                sc.AddTransient<IEventHandler<OrderPlaced>, OrderPlacedTest3EventHandler>();
-            });
-            
-            var @event = new OrderPlaced();
-            
-            sut.Publish(@event);
-            
-            @event.HandlerProcessed.ShouldContain(nameof(OrderPlacedTest1EventHandler));
-            @event.HandlerProcessed.ShouldContain(nameof(OrderPlacedTest2EventHandler));
-            @event.HandlerProcessed.ShouldNotContain(nameof(OrderPlacedTest3EventHandler));
-        }
-        
-        internal class OrderPlaced
-        {
-            public List<string> HandlerProcessed { get; set; } = new List<string>();
-        }
-        
-        internal class OrderPlacedTest1EventHandler : EventHandler<OrderPlaced>
-        {
-            public override void Handle(IRequestBusContext context, OrderPlaced @event)
-            {
-                @event.HandlerProcessed.Add(nameof(OrderPlacedTest1EventHandler));
-            }
-        }
-        
-        internal class OrderPlacedTest2EventHandler : EventHandler<OrderPlaced>
-        {
-            public override void Handle(IRequestBusContext context, OrderPlaced @event)
-            {
-                @event.HandlerProcessed.Add(nameof(OrderPlacedTest2EventHandler));
-            }
-        }
-        
-        internal class OrderPlacedTest3EventHandler : EventHandler<OrderPlaced>
-        {
-            public override void Handle(IRequestBusContext context, OrderPlaced @event)
-            {
-                @event.HandlerProcessed.Add(nameof(OrderPlacedTest3EventHandler));
+                var sut = IocHelper.GetRequestBus(sc =>
+                {
+                    sc.AddTransient<IEventHandler<OrderPlaced>, OrderPlacedTest1EventHandler>();
+                    sc.AddTransient<IEventHandler<OrderPlaced>, OrderPlacedTest2EventHandler>();
+                    sc.AddTransient<IEventHandler<OrderPlaced>, OrderPlacedTest3EventHandler>();
+                });
+
+                var @event = new OrderPlaced();
+
+                sut.Publish(@event);
+
+                @event.HandlersProcessed.ShouldContain(nameof(OrderPlacedTest1EventHandler));
+                @event.HandlersProcessed.ShouldContain(nameof(OrderPlacedTest2EventHandler));
+                @event.HandlersProcessed.ShouldNotContain(nameof(OrderPlacedTest3EventHandler));
             }
 
-            public override bool IsApplicable(IRequestBusContext context, OrderPlaced @event) => false;
+            internal class OrderPlaced
+            {
+                public List<string> HandlersProcessed { get; set; } = new List<string>();
+            }
+
+            internal class OrderPlacedTest1EventHandler : EventHandler<OrderPlaced>
+            {
+                public override void Handle(IRequestBusContext context, OrderPlaced @event)
+                {
+                    @event.HandlersProcessed.Add(nameof(OrderPlacedTest1EventHandler));
+                }
+            }
+
+            internal class OrderPlacedTest2EventHandler : EventHandler<OrderPlaced>
+            {
+                public override void Handle(IRequestBusContext context, OrderPlaced @event)
+                {
+                    @event.HandlersProcessed.Add(nameof(OrderPlacedTest2EventHandler));
+                }
+            }
+
+            internal class OrderPlacedTest3EventHandler : EventHandler<OrderPlaced>
+            {
+                public override void Handle(IRequestBusContext context, OrderPlaced @event)
+                {
+                    @event.HandlersProcessed.Add(nameof(OrderPlacedTest3EventHandler));
+                }
+
+                public override bool IsApplicable(IRequestBusContext context, OrderPlaced @event) => false;
+            }
         }
     }
 }
